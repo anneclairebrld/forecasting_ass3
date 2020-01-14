356b83e7adf4cad0d0c4b15a6e14879b4fb98b59
Anne-Claire Bourland <anneclaire.bourland@gmail.com>
2020-01-14 17:56:19 +0100

    question  1 a and 1.b

--- main.R
+++ main.R
@@ -1,6 +1,8 @@
 library(lubridate)
 library(fpp)
 library(tseries)
+library(ggfortify)
+library(tidyverse)
 
 
 # set current working dir to where the file is
@@ -16,20 +18,26 @@ fred_qd <- fred_qd[3:nrow(fred_qd), ]
 
 fred_qd$sasdate <- quarter(fred_qd$sasdate, with_year = TRUE)
 pc$observation_date <- quarter(pc$observation_date, with_year = TRUE)
-pc$year <- substring(pc$observation_date, 1, 4)
 
-#### QUESTION 1.a ###
+#### QUESTION 1.a ####
 
 # separate training and testing datasets
-test_data <- pc[(pc$year <= 2019) & (pc$year >= 1990), ]
-train_data <- pc[pc$year < 1990, ]
-
+my_ts <- ts(pc$PCND_PCH, frequency = 4, start = c(1942, 2), end = c(2019, 3))
+train_data <- window(my_ts, start = c(1942, 2), end = c(1989, 4))
+test_data <- window(my_ts, start = c(1990, 1), end = c(2019, 3))
+# maybe do seasonal decomposition before 
+  
 # Forecasting AR 1 
-fit <- Arima(train_data$PCND_PCH, c(1, 0, 0),  seasonal=FALSE)
-fcast1 <- forecast(fit)
+fit <- Arima(train_data, c(1, 0, 0))
+fcast1 <- forecast(fit, h = 76) # h is the number of predictions maybe should be 76?
 
-# And plot 1 
-plot(fcast1) ## add correct legend ?
+#### QUESTION 1.b ####
+plot(fcast1, xlab = 'Year', ylab = 'Quartely growth rate of U.S personal consumption\n of non-durable goods') + ## add correct legend ?
+  lines(test_data, col = 'red', lty=2) 
+
+
+  
+
 
 
 
