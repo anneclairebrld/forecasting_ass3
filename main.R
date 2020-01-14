dd541a41483c2030877c7246bb7ef1fe1721c209
Anne-Claire Bourland <anneclaire.bourland@gmail.com>
2020-01-14 15:17:56 +0100

    data clean separation and read in files

--- main.R
+++ main.R
@@ -2,6 +2,14 @@
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 # read data files in 
-fred_qd <-file('./data/FRED-QD.csv', 'r')
-pc <-file('./data/PC.csv', 'r')
+fred_qd <-read.csv('./data/FRED-QD.csv')
+pc <- read.csv('./data/PC.csv')
 
+# separate fred to info and usuable data 
+info <- fred_qd[1:2, ]
+fred_qd <- fred_qd[3:nrow(fred_qd), ]
+
+
+
+#### QUESTION 1.a ###
+head(pc)
