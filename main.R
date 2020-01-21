5672d2ae855ea4d9efbb4b9ba4b8848b37303f70
anneclairebrld <anneclaire.bourland@gmail.com>
2020-01-21 11:42:24 +0100

    questions 1.a and 1.b

--- main.R
+++ main.R
@@ -2,7 +2,6 @@ library(lubridate)
 library(fpp)
 library(tseries)
 library(ggfortify)
-library(tidyverse)
 library(astsa)
 library(zoo)
 
@@ -16,15 +15,16 @@ pc <- read.csv('./data/PC.csv',  sep=';')
 # separate fred to info and usuable data 
 info <- fred_qd[1:2, ]
 fred_qd <- fred_qd[3:nrow(fred_qd), ]
-
+  
 fred_qd$sasdate <- quarter(fred_qd$sasdate, with_year = TRUE)
 pc$observation_date <- quarter(pc$observation_date, with_year = TRUE)
 
 #### QUESTION 1.a ####
 
 # separate training and testing datasets
-my_ts <- ts(pc$PCND_PCH, frequency = 4, start = c(1942, 2))
-train_data <- window(my_ts, start = c(1942, 2), end = c(1989, 4))
+
+my_ts <- ts(pc$PCND_PCH,  frequency = 4, start = c(1947, 2))
+train_data <- window(my_ts, start = c(1947, 2), end = c(1989, 4))
 test_data <- window(my_ts, start = c(1989, 4)) # start here because easier to predict later i.e we start predicting for 1990
 
 ## multistep forecast
@@ -33,23 +33,28 @@ fcast1_cum <- fcast1
 # predict as many times as there are elements in test data 
 for(i in c(1:length(test_data))){
   date <- time(test_data)[i] # get the date 
-  temp <- window(my_ts, start = 1942.25, end = date) # date - 0.75 as we look back 4 quarters and date is included date = what we want to predict - 1 quarter
+  temp <- window(my_ts, end = date) 
   fcast1.update <- arima(temp, c(1, 0, 0)) # fit an AR(1)
   fcast1 <- ts(c(fcast1, forecast(fcast1.update, 1)$mean), start = time(fcast1)[1], frequency = 4) # forecast and add to our forecast array 
-  fcast1_cum <- ts(c(fcast1_cum, sum(forecast(fcast1.update, 4)$mean)), start = time(fcast1)[1], frequency = 4)
+  fcast1_cum <- ts(c(fcast1_cum, sum(forecast(fcast1.update, 4)$mean)), start = time(fcast1)[1], frequency = 4) # maybe this should add the past 4 predicitons instead of predicting the next 4
 }
 
 
-#### QUESTION 1.b ####
-plot(train_data, col = 'blue', xlab = 'Year', 
-     ylab = 'Quartely growth rate of U.S personal consumption\n of non-durable goods',  
-     main='Multistep forecast of personal consumprion of non-durable goods', 
-     xlim = c(1942, 2019))  ## add correct legend ?)
-lines(fcast1, col = 'black') 
-lines(test_data, col = 'red') 
-lines(fcast1_cum, col = 'green')
+## One step ahead forecast
+plot(test_data, col = 'black', xlab = 'Year', ylab = 'Rate of U.S personal consumption of non-durable goods', 
+     main = 'Forecast and realisation from 1990:Q1 to 2019:Q3', 
+     xlim = c(1990, 2020))
+lines(fcast1, col = 'blue')
+legend(x = 'bottomright', legend = c ('Realisation', 'Forecast'), col = c('black', 'blue'), lty = 1:1, cex = 1)
 
-plot(my_ts)
+# cumulative next year forecast 
+plot(test_data, col = 'black', xlab = 'Year', ylab = 'Rate of U.S personal consumption of non-durable goods', 
+     main = 'Forecast and realisation from 1990:Q1 to 2019:Q3', 
+     xlim = c(1990, 2020), 
+     ylim = c(-7, 7))
+lines(fcast1_cum, col = 'blue')
+legend(x = 'bottomright', legend = c ('Realisation', 'Forecast'), col = c('black', 'blue'), lty = 1:1, cex = 1)
+
 
 
 #### QUESTION 1.c ####
