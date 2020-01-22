e4f67c06a2f3f7536ceda83f1c5b1feb6babb094
Camille <camille.morand-duval@student-cs.fr>
2020-01-22 13:18:50 +0100

    question 2.a, b

--- main.R
+++ main.R
@@ -10,27 +10,25 @@ library(missMDA)
 library(vars)
 library(imputeTS)
 library(xts)
+library(tidyr)
+require(dplyr)
 
 # set current working dir to where the file is
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 # read data files in 
 fred_qd <-read.csv('./data/FRED-QD.csv')
-pc <- read.delim('./data/PC.csv',';', header = TRUE)
+pc <- read.csv('./data/PC.csv',  sep=';')
+
+names(pc)[1] <- "observation_date"
 
 # separate fred to info and usuable data 
 info <- fred_qd[1:2, ]
 fred_qd <- fred_qd[3:nrow(fred_qd), ]
-  
-fred_qd$sasdate <- quarter(fred_qd$sasdate, with_year = TRUE)
-pc$observation_date <- quarter(pc$observation_date, with_year = TRUE)
-fred_qd$sasdate <- as.Date(fred_qd$sasdate, format='%m/%d/%Y')
-# fred_qd$year <- as.numeric(format(fred_qd$sasdate, '%Y'))
 
-# fred_qd$quarter[as.numeric(format(fred_qd$sasdate, '%m')) == 3] <- 'Q1'
-# fred_qd$quarter[as.numeric(format(fred_qd$sasdate, '%m')) == 6] <- 'Q2'
-# fred_qd$quarter[as.numeric(format(fred_qd$sasdate, '%m')) == 9] <- 'Q3'
-# fred_qd$quarter[as.numeric(format(fred_qd$sasdate, '%m')) == 12] <- 'Q4'
+# fred_qd$sasdate <- quarter(fred_qd$sasdate, with_year = TRUE)
+pc$observation_date <- quarter(pc$observation_date, with_year = TRUE)
+fred_qd$sasdate <- as.Date(fred_qd$sasdate, format = "%m/%d/%Y")
 
 #### QUESTION 1.a ####
 
@@ -38,10 +36,11 @@ fred_qd$sasdate <- as.Date(fred_qd$sasdate, format='%m/%d/%Y')
 
 my_ts <- ts(pc$PCND_PCH,  frequency = 4, start = c(1947, 2))
 train_data <- window(my_ts, start = c(1947, 2), end = c(1989, 4))
-test_data <- window(my_ts, start = c(1989, 4)) # start here because easier to predict later i.e we start predicting for 1990
+test_data <- window(my_ts, start = c(1989, 4), end = c(2019, 3)) # start here because easier to predict later i.e we start predicting for 1990
 
 ## multistep forecast
 fcast1 <- as.ts(as.zoo(train_data)[(length(train_data) - 1):length(train_data)]) # transformed back to ts
+
 fcast1_cum <- fcast1
 # predict as many times as there are elements in test data 
 for(i in c(1:length(test_data))){
@@ -54,7 +53,6 @@ for(i in c(1:length(test_data))){
 
 #### QUESTION 1.b ####
 
-#### QUESTION 1. b ####
 ## One step ahead forecast
 plot(test_data, col = 'black', xlab = 'Year', ylab = 'Rate of U.S personal consumption of non-durable goods', 
      main = 'Forecast and realisation from 1990:Q1 to 2019:Q3', 
@@ -123,29 +121,46 @@ imp <- na_ma(ACONGNOx_ts, weighting = "exponential", maxgap = Inf)
 plotNA.imputations(ACONGNOx_ts, imp)
 
 # missMDA package - PCA and missing value for multivariate
-# CHECK THIS IS A REAL PCA AND NOT SVD
-data(fred_qd)
+# completes the data without impacting the outputted PCA
 nb = estim_ncpPCA(fred_qd[,-1], ncp.max = 5)
 res.imp = imputePCA(fred_qd[,-1],ncp=nb$ncp)
 fred_pca = PCA(res.imp$completeObs)
-principal_comp = fred_pca$ind$coord
-
-#http://factominer.free.fr/course/missing.html
-#https://cran.r-project.org/web/packages/missMDA/missMDA.pdf
-#https://otexts.com/fpp2/#
-#https://cloud.r-project.org/web/packages/imputeTS/vignettes/imputeTS-Time-Series-Missing-Value-Imputation-in-R.pdf
-
-#-- Choosing the number of principal components literature
-#https://cran.r-project.org/web/packages/PCDimension/vignettes/PCDimension.pdf
+summary(fred_pca) # only three dimensions necessary 
 
 ## question 2.b
-# factors <- cbind(FAVAR_T[c("Date")], FAVAR_PCA$x)
+factors = data.frame(fred_pca$ind$coord) # extract 5 principal components
 
-factors = data.frame(fred_pca$ind$coord)
-model <- VAR(factors, p=12)
-summary(model)
-prd <- predict(model, n.ahead = 10, ci = 0.95, dumvar = NULL)
+# using pc - WRONG SHOULD BE ct
+# pc$observation_date = as.Date(as.yearqtr(pc$observation_date, format = "%y.%Q"))
+# PC <- pc %>% filter(observation_date >= "1959-01-01", observation_date < "2019-09-01")
+# variables <- cbind(PC, factors)
+
+# using ct
+ct <- forecast(fcast1.update, 1)
+ct <- data.frame(Y=as.matrix(ct$x), date=as.Date(as.yearmon(time(ct$x))))
+ct <- ct %>% filter(date >= "1959-01-01", date <= "2019-07-01")
+variables <- cbind(ct, factors)
+
+VARselect(variables[,-2], lag.max = 12, type = "const") # change to variables [,-1] if using pc
+VARmodel <- VAR(variables[,-2], p=2) # from VARselect
+summary(VARmodel)
+
+# question 2.c
+for(i in c(1:length(test_data))){
+  date <- time(test_data)[i] # get the date 
+  temp <- window(my_ts, end = date) 
+  # fcast2.update <- arima(temp, c(1, 0, 0)) # fit an AR(1)
+  fcast1 <- ts(c(fcast1, forecast(VARmodel, 1)$mean), start = time(fcast1)[1], frequency = 4) # forecast and add to our forecast array 
+  fcast1_cum <- ts(c(fcast1_cum, sum(forecast(fcast1.update, 4)$mean)), start = time(fcast1)[1], frequency = 4) # maybe this should add the past 4 predicitons instead of predicting the next 4
+}
+
+a=forecast(VARmodel$datamat, 1)
+
+
+
+prd <- predict(VARmodel, n.ahead = 10, ci = 0.95, dumvar = NULL)
 print(prd)
 plot(prd, "single")
 
+FORCAST1 = data.frame(Y=as.matrix(fcast1), date=as.Date(as.yearmon(time(fcast1))))
 
